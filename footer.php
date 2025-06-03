3c370b2926d1a2a23e40cc74ec11199bf7bffbdc
Klein <202310281@fit.edu.ph>
2025-06-03 11:09:50 +0800

    updated the main

--- footer.php
+++ footer.php
@@ -1,10 +1,19 @@
-<h2>All Weather Conditions</h2>
-        <ul>
+<ul>
             <?php
-            $weatherConditions = ['Sunny', 'Rainy', 'Cloudy', 'Snowy'];
-            foreach ($weatherConditions as $condition) {
-                echo "<li>$condition</li>";
+            function displayBooks($books) {
+                foreach ($books as $book) {
+                    echo "<li>{$book['title']} by {$book['author']}</li>";
+                }
             }
+
+            $books = [
+                ['title' => 'The Great Gatsby', 'author' => 'F. Scott Fitzgerald'],
+                ['title' => 'Don Quixote', 'author' => 'Miguel de Cervantes'],
+                ['title' => 'To Kill a Mockingbird', 'author' => 'Harper Lee'],
+                ['title' => 'Pride and Prejudice', 'author' => 'Jane Austen']
+            ];
+
+            displayBooks($books);
             ?>
         </ul>
     </div>
